c512ac6ff770214ed99a1bb74b4168015e88a02f
DavidPT <davidpt94@gmail.com>
2024-05-06 06:47:35 -0500

    Create folder project exagonal structure

---
+++ src/main/java/com/riwiEcomerce/riwiEcomerce/RiwiEcomerceApplication.java
@@ -0,0 +1,13 @@
+package com.riwiEcomerce.riwiEcomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiEcomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiEcomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwiEcomerce/riwiEcomerce/RiwiEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwiEcomerce.riwiEcomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
