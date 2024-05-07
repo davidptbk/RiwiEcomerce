2d208be14169f5e8843512aceb84b7f36f252fea
DavidPT <davidpt94@gmail.com>
2024-05-07 16:11:34 -0500

    Feature/ create entity User

--- src/main/java/com/riwi_ecomerce/riwi_ecomerce/domain/entities/UserEntity.java
+++ src/main/java/com/riwi_ecomerce/riwi_ecomerce/domain/entities/UserEntity.java
@@ -1,5 +1,31 @@
 package com.riwi_ecomerce.riwi_ecomerce.domain.entities;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    private String phone;
+    private String riwiPoints;
 }
